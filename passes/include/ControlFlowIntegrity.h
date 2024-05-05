749dd0a33fd8ecd583f0274bad82a9a7d8a42563
Robert Klink <roberthklink@gmail.com>
2024-05-05 18:13:52 +0000

    add: outline `ReturnGraph`

--- passes/include/ControlFlowIntegrity.h
+++ passes/include/ControlFlowIntegrity.h
@@ -14,13 +14,21 @@
 #include "llvm/IR/PassManager.h"
 #include "llvm/Pass.h"
 
+#include <map>
+
 //------------------------------------------------------------------------------
 // New PM interface
 //------------------------------------------------------------------------------
 struct ControlFlowIntegrity : public llvm::PassInfoMixin<ControlFlowIntegrity> {
+    // typedef for the return graph
+    typedef std::map<llvm::Function *, std::set<llvm::Function *>> ReturnGraph;
+
+    // Function to convert the call graph to a return graph
+    ReturnGraph GetReturnGraph(llvm::Module &M, llvm::CallGraph &CG);
+
     llvm::PreservedAnalyses run(llvm::Module &M,
                                 llvm::ModuleAnalysisManager &AM);
-    bool runOnModule(llvm::Module &M, llvm::CallGraphAnalysis::Result &CG);
+    bool runOnModule(llvm::Module &M, ReturnGraph &RG);
 
     // Without isRequired returning true, this pass will be skipped for
     // functions decorated with the optnone LLVM attribute. Note that clang -O0
--- passes/lib/ControlFlowIntegrity.cpp
+++ passes/lib/ControlFlowIntegrity.cpp
@@ -13,11 +13,14 @@
 //========================================================================
 #include "ControlFlowIntegrity.h"
 
+#include "llvm/ADT/SCCIterator.h"
 #include "llvm/Analysis/CallGraph.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/InstIterator.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
+
+#include <map>
 
 using namespace llvm;
 
@@ -26,19 +29,48 @@ using namespace llvm;
 //-----------------------------------------------------------------------------
 // ControlFlowIntegrity implementation
 //-----------------------------------------------------------------------------
+ControlFlowIntegrity::ReturnGraph
+ControlFlowIntegrity::GetReturnGraph(Module &M, CallGraph &CG) {
+    ControlFlowIntegrity::ReturnGraph RG;
+
+    for (auto IT = scc_begin(&CG), IE = scc_end(&CG); IT != IE; ++IT) {
+        // Get the current SCC and iterate over each node in the SCC
+        const std::vector<CallGraphNode *> &SCC = *IT;
+        for (auto *CGN : SCC) {
+            // Get the caller function and iterate over each callee
+            Function *Caller = CGN->getFunction();
+            if (!Caller)
+                continue;
+            for (auto &E : *CGN) {
+                // Get the callee and insert the caller into the set of callers
+                Function *Callee = E.second->getFunction();
+                RG[Callee].insert(Caller);
+            }
+        }
+    }
+
+    return RG;
+}
+
 bool ControlFlowIntegrity::runOnModule(Module &M,
-                                       CallGraphAnalysis::Result &CG) {
+                                       ControlFlowIntegrity::ReturnGraph &RG) {
     bool insertedChecks = false;
 
     for (Function &F : M) {
         for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
             if (auto *CB = dyn_cast<CallBase>(&*I)) {
                 // We have a call to a function, insert check that call is to
-                // valid target (using call graph), else panic
-                errs() << *CB->getCalledOperand() << "\n";
+                // valid target then either
+                //   - actually go to function if valid target
+                //   - panic if not valid
+
+                if (!CB->isIndirectCall())
+                    continue;
+
             } else if (auto *RI = dyn_cast<ReturnInst>(&*I)) {
-                // We have a return, insert check that the return is to a valid
-                // target (using call graph), else panic
+                // TODO: ask on how to handle backward edge CFI
+                //   - ShadowStack
+                //   - ReturnAddress (need to modify X86)
             }
         }
     }
@@ -49,7 +81,8 @@ bool ControlFlowIntegrity::runOnModule(Module &M,
 PreservedAnalyses ControlFlowIntegrity::run(llvm::Module &M,
                                             llvm::ModuleAnalysisManager &AM) {
     auto &CG = AM.getResult<CallGraphAnalysis>(M);
-    bool changed = runOnModule(M, CG);
+    auto RG = ControlFlowIntegrity::GetReturnGraph(M, CG);
+    bool changed = runOnModule(M, RG);
 
     return (changed ? llvm::PreservedAnalyses::none()
                     : llvm::PreservedAnalyses::all());
