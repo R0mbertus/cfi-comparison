26ef864ed65cab3f4e2cc893805f1aeebee6451a
Robert Klink <roberthklink@gmail.com>
2024-05-29 21:57:57 +0200

    refactor: see previous

--- passes/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p build
-cd build
-cmake -DLT_LLVM_INSTALL_DIR=$LLVM_DIR ..
-make
--- passes/test/direct.cpp
+++
@@ -1,9 +0,0 @@
-int func2(void) { return 1 + 1; }
-
-int func1(void) { return 1 + 2; }
-
-int main(void) {
-    func1();
-    func2();
-    return 0;
-}
